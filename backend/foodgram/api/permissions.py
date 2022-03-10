ff132fc4ba538e80a8b27b3d94ceefaeb900e3aa
Darya <lunaticmoony13@gmail.com>
2022-03-10 21:05:00 +0300

    Fixes as per review 10 March

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -13,9 +13,6 @@ class IsAuthenticated(permissions.IsAuthenticated):
 class ReadOnly(permissions.BasePermission):
     """Разрешает доступ к безопасным методам."""
 
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
-
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS
 
@@ -31,12 +28,6 @@ class IsAdmin(permissions.BasePermission):
     """Разрешает доступ администратору."""
 
     message = 'Доступ только для администрации.'
-
-    def has_permission(self, request, view):
-        return bool(
-            request.user.role == User.ADMIN
-            or request.user.is_superuser
-        )
 
     def has_object_permission(self, request, view, obj):
         return bool(
